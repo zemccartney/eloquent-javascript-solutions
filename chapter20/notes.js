98b78f80c46c422b8c6bbc8a805ea8459f676bce
Zack McCartney <zemccartney@gmail.com>
2017-08-11 21:44:54 -0400

    Restarted ch.20

--- chapter20/notes.js
+++ chapter20/notes.js
@@ -1,9 +1,17 @@
-// var hello = "Hello!";
-// console.log(hello);
+/*var mkl = require('test_mod');
+var hello = "Hello!";
+console.log(hello);
+
+
+
+console.log(process.argv);
+console.log(global);
+process.exit(0);*/
 
 var garble = require("./garble");
 
 var argument = process.argv[2];
+var garbled = garble.garble(argument);
 
-console.log(garble(argument));
-console.log("fart".split(""));
+console.log(garbled);
+console.log(garble.translate(garbled));
